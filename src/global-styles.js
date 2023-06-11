bdb1e008e1edec46dc881345e978f6ef97b37291
thomasdev9 <61947882+thomasdev9@users.noreply.github.com>
2023-06-11 20:46:30 +0300

    Setup eslint and prettier (#4)

--- src/global-styles.js
+++ src/global-styles.js
@@ -1,4 +1,4 @@
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyles = createGlobalStyle`
     *{
@@ -10,6 +10,6 @@ const GlobalStyles = createGlobalStyle`
     body{
         font-family: "Poppins", sans-serif;
     }
-`
+`;
 
 export default GlobalStyles;
