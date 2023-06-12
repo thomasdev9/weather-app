3a316a89504d119a3d6105f641b50b90fe382ce6
thomasdev9 <61947882+thomasdev9@users.noreply.github.com>
2023-06-12 13:38:57 +0300

    Header component (#10)

--- src/components/content.js
+++ src/components/content.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import Header from './header';
 
 const Wrapper = styled.div`
   width: 100%;
@@ -15,15 +16,27 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: flex-start;
+  justify-content: center;
   width: 50%;
   height: auto;
+  padding: 50px 0px;
+
+  @media only screen and (max-width: 800px) {
+    width: 80%;
+  }
+
+  @media only screen and (max-width: 600px) {
+    width: 100%;
+    padding: 50px 25px;
+  }
 `;
 
 function Content() {
   return (
     <Wrapper>
-      <Container></Container>
+      <Container>
+        <Header />
+      </Container>
     </Wrapper>
   );
 }
