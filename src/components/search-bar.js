51b27627aa13ac3cf49f616ec56d1568af5241b2
thomas <makis7olymp@yahoo.gr>
2023-06-13 17:00:05 +0300

    Error message for no location

--- src/components/search-bar.js
+++ src/components/search-bar.js
@@ -14,13 +14,30 @@ const Conntainer = styled.div`
   }
 `;
 
+const IconButton = styled.button`
+  background: transparent;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  width: 40px;
+  height: 40px;
+  border: none;
+`;
+
 const StyledIcon = styled(FaSearch)`
   color: #5151e5;
   cursor: pointer;
 `;
 
+const ErrorMessage = styled.h3`
+  font-weight: 500;
+  color: rgb(255, 255, 255);
+`;
+
 function SearchBar() {
   const [value, setValue] = useState('');
+  const [selectedValue, setSelectedValue] = useState('');
   const suggestions = [
     {
       key: 'london',
@@ -108,20 +125,28 @@ function SearchBar() {
     },
   ];
 
-  console.log(value);
+  const handleSelect = (record) => {
+    setSelectedValue(record?.item?.value);
+  };
 
   return (
     <Conntainer>
       <ReactSearchBox
-        leftIcon={<StyledIcon />}
+        leftIcon={
+          <IconButton>
+            <StyledIcon />
+          </IconButton>
+        }
         iconBoxSize="48px"
         onChange={(value) => {
           setValue(value);
         }}
+        onSelect={(record) => handleSelect(record)}
         data={suggestions}
         inputFontColor="#414856"
         placeholder="Search city in UK..."
       />
+      {value?.length > 0 && selectedValue === '' && <ErrorMessage>Unfortunately, no loaction is found.</ErrorMessage>}
     </Conntainer>
   );
 }
