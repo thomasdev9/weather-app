c3a032c6275c66c0d03404c5a711ab29a7a16239
thomas <makis7olymp@yahoo.gr>
2023-06-14 16:35:11 +0300

    Provide redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { combineReducers } from 'redux';
 import currentReducers from './slices/currentSlice';
+import forecastReducers from './slices/forecastSlice';
 
 const reducer = combineReducers({
   current: currentReducers,
+  forecast: forecastReducers,
 });
 
 export const store = configureStore({
