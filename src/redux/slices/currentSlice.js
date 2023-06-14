a32f21bf11f71c39ffa1f5e036a18791e70d6f25
thomas <makis7olymp@yahoo.gr>
2023-06-14 16:23:55 +0300

    Redux reducers

--- src/redux/slices/currentSlice.js
+++ src/redux/slices/currentSlice.js
@@ -1,4 +1,5 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { getCurrent } from '../../api/weather-api';
 
 const initialState = {
   data: {},
@@ -7,10 +8,29 @@ const initialState = {
   success: false,
 };
 
+export const fetchCurrent = createAsyncThunk('current/fetchCurrent', async ({ lat, lon }) => {
+  const response = await getCurrent({ lat, lon });
+  return response;
+});
+
 const currentSlice = createSlice({
   name: 'current',
   initialState,
   reducers: {},
+  extraReducers(builder) {
+    builder.addCase(fetchCurrent.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(fetchCurrent.fulfilled, (state, action) => {
+      state.loading = false;
+      state.data = action.payload;
+      state.success = true;
+    });
+    builder.addCase(fetchCurrent.rejected, (state, action) => {
+      state.loading = false;
+      state.error = action?.error?.message;
+    });
+  },
 });
 
 export default currentSlice.reducer;
--- src/redux/slices/forecastSlice.js
+++ src/redux/slices/forecastSlice.js
@@ -1,4 +1,5 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { getForecast } from '../../api/weather-api';
 
 const initialState = {
   data: {},
@@ -7,10 +8,29 @@ const initialState = {
   success: false,
 };
 
+export const fetchForecast = createAsyncThunk('current/fetchForecastWeather', async ({ lat, lon, cnt }) => {
+  const response = await getForecast({ lat, lon, cnt });
+  return response;
+});
+
 const forecastSlice = createSlice({
   name: 'forecast',
   initialState,
   reducers: {},
+  extraReducers(builder) {
+    builder.addCase(fetchForecast.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(fetchForecast.fulfilled, (state, action) => {
+      state.loading = false;
+      state.data = action?.payload;
+      state.success = true;
+    });
+    builder.addCase(fetchForecast.rejected, (state, action) => {
+      state.loading = false;
+      state.error = action?.error?.message;
+    });
+  },
 });
 
 export default forecastSlice.reducer;
